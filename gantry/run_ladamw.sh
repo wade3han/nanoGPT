d25be5335f3dd64c54accd76facfe205e768dd55
Seungju Han <wade3han@gmail.com>
2024-08-15 20:40:33 +0900

    update

--- gantry/run_ladamw.sh
+++ gantry/run_ladamw.sh
@@ -7,11 +7,13 @@ echo "Training llama model using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size 
 # You can also set --gradient_checkpointing or use `stage3_offloading_accelerate.conf` to save memory,
 # but it will trade off speed.
 # sweep learning rate from 2e-5 to 1e-6
-NAME=gemma2_2b_adamw_lr2e-5_seq2048
+NAME=ladamw
 
-gantry run --beaker-image seungjuh/open-instruct-public-240806-preview --venv base --name $NAME --cluster ai2/pluto-cirrascale --workspace ai2/safety --pip requirements.txt \
+export WANDB_ENTITY=seungjuhan3
+gantry run --beaker-image seungjuh/open-instruct-public-240806-preview --venv base --name $NAME \
+  --cluster ai2/pluto-cirrascale --workspace ai2/safety --pip requirements.txt \
   --gpus $NUM_GPUS --priority normal \
   --preemptible --env-secret WANDB_API_KEY=WANDB_API_KEY \
-  --env-secret HF_TOKEN=HUGGING_FACE_HUB_TOKEN --env WANDB_PROJECT=llama2-finetuning \
-  --env WANDB_ENTITY=seungjuhan3 --env WANDB_NAME=$NAME --env-secret OPENAI_API_KEY=openai_api_key --budget ai2/oe-adapt -- \
+  --env-secret HF_TOKEN=HUGGING_FACE_HUB_TOKEN \
+  --env WANDB_ENTITY=seungjuhan3 --env-secret OPENAI_API_KEY=openai_api_key --budget ai2/oe-adapt -- \
   torchrun --standalone --nproc_per_node=4 train.py config/train_gpt2_4gpu_ladamw.py
